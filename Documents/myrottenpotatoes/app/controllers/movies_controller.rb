68876241dbf75c05d252b4614004bfcea5d3e63c
saasbook <saasbook@saasbook.(none)>
2015-03-01 17:56:52 -0400

    comment

--- Documents/myrottenpotatoes/app/controllers/movies_controller.rb
+++ Documents/myrottenpotatoes/app/controllers/movies_controller.rb
@@ -16,4 +16,11 @@ end
 def new
 	# default: render 'new' template
 end
+
+def create
+	@movie = Movie.create!(params[:movie])
+	flash[:notice] = "#{@movie.title} was successfully created."
+	redirect_to movies_path
+end
+
 end
